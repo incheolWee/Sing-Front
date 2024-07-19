7e2907cbeb76818baad3c327e3e8b3c36c4aa4ff
dnldl <dnldlscjf@gmail.com>
2024-07-19 11:34:51 +0900

    Merge Main

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import WorkPage from "./pages/work_page";
 import SignPage from "./pages/sign_page";
 import LandingPage from "./pages/landing_page";
 import PDFSignatureApp from "./pages/test_page";
+import LandingPage from "./pages/landing_page";
 
 function App() {
   return (
@@ -14,10 +15,12 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/home" element={<HomePage />} />
         <Route path="/work" element={<WorkPage />} />
-        <Route path="/work" element={<SignPage />} />
+
+        <Route path="/sign" element={<SignPage />} />
+        <Route path="/test" element={<PDFSignatureApp />} />
       </Route>
       <Route path="/" element={<LandingPage />} />
-    </Routes>
+    </Routes >
   );
 }
 
